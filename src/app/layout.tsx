d55f777c7c21265a14e0711c7dea8c1bbf670cfc
akinyiliz <lizzieke84@gmail.com>
2023-09-07 11:49:11 +0300

    Added Toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/Toast";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
@@ -18,10 +19,11 @@ export default function RootLayout({
     >
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers>
-          {children}
-
           {/* @ts-expect-error Server Component */}
           <Navbar />
+          <Toaster position="top-right" />
+
+          <main>{children}</main>
         </Providers>
 
         {/* Allow for more height on mobile devices */}
