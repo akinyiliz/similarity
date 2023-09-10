9709bbd54b8a7a124339bd6ec54ba6a73992e33f
akinyiliz <lizzieke84@gmail.com>
2023-09-10 17:36:08 +0300

    Code

---
+++ src/components/ui/Code.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Highlight, themes, type Language } from "prism-react-renderer";
+import { useTheme } from "next-themes";
+
+interface CodeProps {
+  code: string;
+  show: boolean;
+  language: Language;
+  animationDelay?: number;
+  animated?: boolean;
+}
+
+const Code: React.FC<CodeProps> = ({
+  code,
+  show,
+  language,
+  animated,
+  animationDelay,
+}) => {
+  const { theme: applicationTheme } = useTheme();
+  const [text, setText] = useState(animated ? "" : code);
+
+  useEffect(() => {
+    if (show && animated) {
+      let i = 0;
+      setTimeout(() => {
+        const intervalId = setInterval(() => {
+          setText(code.slice(0, i));
+          i++;
+
+          if (i > code.length) {
+            clearInterval(intervalId);
+          }
+        }, 15);
+
+        return () => clearInterval(intervalId);
+      }, animationDelay || 150);
+    }
+  }, [code, show, animated, animationDelay]);
+
+  // Number of lines in the code
+  const lines = text.split(/\r\n|\r|\n/).length;
+
+  const lightTheme = themes.vsLight;
+  const darkTheme = themes.nightOwl;
+
+  const theme = applicationTheme === "light" ? lightTheme : darkTheme;
+
+  return (
+    <Highlight code={text} language={language} theme={theme}>
+      {({ className, tokens, getLineProps, getTokenProps }) => (
+        <pre
+          className={
+            className +
+            "transition-all w-fit bg-transparent duration-100 py-0 no-scrollbar"
+          }
+          style={{
+            maxHeight: show ? lines * 24 : 0,
+            opacity: show ? 1 : 0,
+          }}
+        >
+          {tokens.map((line, i) => {
+            // eslint-disable-next-line no-unused-vars
+            const { key, ...rest } = getLineProps({ line, key: i });
+            return (
+              <div key={`line-${i}`} style={{ position: "relative" }} {...rest}>
+                {line.map((token, index) => {
+                  // eslint-disable-next-line no-unused-vars
+                  const { key, ...props } = getTokenProps({ token, i });
+                  return <span key={index} {...props} />;
+                })}
+              </div>
+            );
+          })}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
+
+export default Code;
